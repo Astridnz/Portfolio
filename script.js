608ec5dd6c0d6bdeb918a65eb8251c02653571b6
astrid_nz <astrid.segard@gmail.com>
2025-04-10 15:53:01 +0200

    update responsive & dark mode

--- script.js
+++ script.js
@@ -5,22 +5,25 @@ const body = document.querySelector("body");
 const toggleCheckbox = document.querySelector(".toggle-checkbox");
 const toggleSlot = document.querySelector(".toggle-slot")
 const welcomeTitle = document.querySelector(".title")
+const poem = document.querySelector(".poem")
 
 // Fonction pour appliquer le mode en fonction de l'état
 const applyMode = (isDarkMode) => {
     if (isDarkMode) {
         body.classList.remove("light-mode");
         body.classList.add("dark-mode");
-        toggleSlot.style.boxShadow = "0px calc(10px / 3) calc(25px / 3) #07215b";
-        welcomeTitle.style.background = `url("/assets/img/moon.jpg") repeat`;
+        toggleSlot.style.boxShadow = "0px calc(10px / 3) calc(25px / 3) #0F056B";
+        welcomeTitle.style.background = `url("/assets/img/dark-mode-title.jpg") center repeat`;
         welcomeTitle.style.backgroundClip = "text";
+        poem.style.backgroundImage = `url("/assets/img/pexels-rakicevic-nenad-233369-1262304.jpg")`;
         toggleCheckbox.checked = true; // Cocher le toggle pour le mode sombre
     } else {
         body.classList.remove("dark-mode");
         body.classList.add("light-mode");
-        toggleSlot.style.boxShadow = "0px calc(10px / 3) calc(25px / 3) #ffbb52";
-        welcomeTitle.style.background = `url("/assets/img/sun.jpg") repeat center`;
+        toggleSlot.style.boxShadow = "0px calc(10px / 3) calc(25px / 3) #fcb252";
+        welcomeTitle.style.background = `url("/assets/img/matin.jpg") repeat center`;
         welcomeTitle.style.backgroundClip = "text";
+        poem.style.backgroundImage = `url(/assets/img/pexels-ryank-17731679.jpg)`;
         toggleCheckbox.checked = false; // Décocher le toggle pour le mode clair
     }
 };
